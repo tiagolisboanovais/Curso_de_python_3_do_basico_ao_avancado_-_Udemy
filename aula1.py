3957b164808cde8e0098fdf65415818b88c49aa3
tiagolisboanovais <tiagolisboanovais@outlook.com>
2023-04-06 07:20:49 -0300

    commit inicial do curso - aula 1

---
+++ aula1.py
@@ -0,0 +1,8 @@
+# Permite escrever um comentário
+print(123)
+
+"""
+Este docstrings as vezes pode ser utilizado como um comentário multilinha em python. 
+São usados 3 aspas seguidas. Pode ser usado aspas duplas ou aspas simples.
+
+"""
