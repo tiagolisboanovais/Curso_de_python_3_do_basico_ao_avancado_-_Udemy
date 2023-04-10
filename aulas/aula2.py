c6d45445b3651c11ad4a4299c0e9b48c25d9b50f
tiagolisboanovais <tiagolisboanovais@outlook.com>
2023-04-10 05:45:36 -0300

    Adicionando informações no arquivo aula2.py

---
+++ aulas/aula2.py
@@ -0,0 +1,11 @@
+print(12, 34)
+print(56, 78)
+
+# Nomeando outro separador para função print
+
+print(56, 78, sep=' ')
+
+# Nomeando final para função print "end"
+
+print(1, 2, sep=' ', end=' ')
+print(3, 4, sep=' ')
